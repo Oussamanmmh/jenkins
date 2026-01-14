d4f9a25bcf5cc5082a0e2f4f6b9ae4912783dac9
oussamanmmh <mo_nemamcha@esi.dz>
2026-01-14 09:34:52 +0100

    test jenkins

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,15 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "Features",plugin={
+        "pretty",
+        "html:build/reports/cucumber/index.html",
+        "json:build/reports/cucumber/json-report.json"
+
+}
+)
+public class ExampleTest {
+
+}
